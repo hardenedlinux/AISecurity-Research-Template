a5986cca5ebe45e5e2e3a06102a4ec33ff519b18
guangtao <gtrunsec@hardenedlinux.org>
2023-12-26 21:16:12 -0800

    feat(jupyenv): add julia kernel

--- nix/lock/flake.nix
+++ nix/lock/flake.nix
@@ -1,4 +1,9 @@
 {
-  inputs = { };
+  inputs = {
+    juliaRegistry.url = "github:CodeDownIO/General";
+    juliaRegistry.flake = false;
+
+    nixpkgs-julia.url = "github:GTrunSec/nixpkgs/julia";
+  };
   outputs = _: { };
 }
--- nix/std/cells/repo/jupyenv/example.nix
+++ nix/std/cells/repo/jupyenv/example.nix
@@ -7,11 +7,44 @@ let
   inherit (inputs) main;
 in
 {
+  nixpkgs = inputs.nixpkgs;
   imports = [ omnibus.jupyenv.quarto ];
-  publishers.quarto.enable = true;
+  publishers.quarto = {
+    enable = true;
+    runtimeInputs = [
+      pkgs.librsvg
+      pkgs.tectonic
+    ];
+  };
 
   kernel.python.data-science = {
     enable = true;
     env = inputs.main.devShells.default.passthru.pythonEnv;
+  };
+  kernel.bash.data-science = {
+    enable = true;
+    runtimePackages = [ pkgs.d2 ];
+  };
+  kernel.julia.data-science = {
+    enable = true;
+    nixpkgs = inputs.nixpkgs-julia;
+    override = {
+      augmentedRegistry = inputs.juliaRegistry.outPath;
+      makeWrapperArgs = "--prefix LD_LIBRARY_PATH : /run/opengl-driver/lib:/run/opengl-driver-32/lib";
+      precompile = false;
+      # artifactsOverrides = {
+      #   "7e76a0d4-f3c7-5321-8279-8d96eeed0f29" = ["libglvnd"];
+      # };
+    };
+    extraJuliaPackages = [
+      "Plots"
+      "GLMakie"
+      "DataFrames"
+    ];
+    extraKernelSpc = {
+      env = {
+        JULIA_NUM_THREADS = "8";
+      };
+    };
   };
 }
