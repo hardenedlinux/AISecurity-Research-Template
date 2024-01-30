9c2e6fea4877a0a821a2442998cd306760bc12b0
guangtao <gtrunsec@hardenedlinux.org>
2024-01-30 15:09:39 -0800

    chore: update locks

--- nix/my-project.nix
+++ nix/my-project.nix
@@ -12,20 +12,17 @@ in
   imports = [ dream2nix.modules.dream2nix.WIP-python-pdm ];
   pdm.lockfile = src + /pdm.lock;
   pdm.pyproject = src + /pyproject.toml;
-  pdm.pythonInterpreter = config.deps.python3;
   mkDerivation = {
     inherit src;
-    buildInputs = [ config.deps.python3.pkgs.pdm-backend ];
+    buildInputs = [ config.deps.python.pkgs.pdm-backend ];
   };
   deps =
     { nixpkgs, ... }:
     {
+      python = nixpkgs.python310;
       mkShell =
         let
-          pythonEnv =
-            (config.deps.python.withPackages (
-              ps: config.mkDerivation.propagatedBuildInputs
-            ));
+          pythonEnv = (config.deps.python.withPackages (ps: config.mkDerivation.propagatedBuildInputs));
         in
         nixpkgs.mkShell {
           buildInputs = [
